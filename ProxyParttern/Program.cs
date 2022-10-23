8924765938e4f34032891a0d9b7949daa414db06
LeVanHieu0509 <LeVanHieu0509@gmail.com>
2022-10-23 19:12:58 +0700

    ProxyParttern

---
+++ ProxyParttern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyParttern
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*Proxy Pattern Demo.***\n");
+            Subject proxy = new Proxy();
+            proxy.DoSomeWork();
+            Console.ReadKey();
+        }
+    }
+}
