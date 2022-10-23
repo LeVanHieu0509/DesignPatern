108dc20f4ec4a940bb77de8fe12d8a7a9d50c18b
LeVanHieu0509 <LeVanHieu0509@gmail.com>
2022-10-23 19:10:31 +0700

    AdaperPartern

---
+++ AdapterParttern/Android.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterParttern
+{
+    public class Android
+    {
+        private bool connector;
+        public void useLightning()
+        {
+            connector = true;
+            Console.WriteLine("microUsb connected");
+        }
+
+        public void recharge()
+        {
+            if (connector)
+            {
+                Console.WriteLine("Recharge started");
+                Console.WriteLine("Recharge finished");
+            }
+            else
+            {
+                Console.WriteLine("Connect microUsb first");
+            }
+        }
+    }
+}
+}
